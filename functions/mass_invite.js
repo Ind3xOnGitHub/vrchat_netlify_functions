4030cf2c4800e7b1947316704ee2957aba3986a3
Ind3xOnGitHub <me@ind3x.site>
2018-11-02 12:07:01 +0100

    Delete mass_invite.js

--- functions/mass_invite.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs');
-
-exports.handler = function(event, context, callback) {
-  callback(null, {
-    statusCode: 200,
-    body: fs.readFileSync(__dirname + '/mass_invite_template.html', 'utf8')
-  })
-}
